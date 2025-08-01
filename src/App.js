5d343fa05cd20655d750b4652def3678b65258c3
Darshana752 <malithdarshana2000@gmail.com>
2025-08-01 20:07:36 +0530

    gfue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  //let count = 0;
+
+  const increment = () => {
+    //count += 1;
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    //count -= 1;
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //using dynamic component
+    <>
+      <span className="title">My Counter</span>
+
+      <p className="subtitle">The count is {count}</p>
+      <button onClick={decrement} className="btn">
+        -
+      </button>
+      <button onClick={increment} className="btn">
+        +
+      </button>
+    </>
   );
 }
 
